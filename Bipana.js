5e95e7f15ef3f6e18151bb03e728cc80177adf47
Bipana <bipana4@gmail.com>
2025-05-06 07:50:22 +0545

    tb

--- Bipana.js
+++ Bipana.js
@@ -1,1 +1,19 @@
+     const hamburger=document.getElementById('hamburger');
+     const navlinks=document.getElementById('navlinks');
+     hamburger.addEventListener('click',()=>{
+        navlinks.classlist.toggle('active');
+     });
      
+    const text =  "Hii, I'm Bipana";
+     const texthii = document.getElementById('texthii');
+     let index = 0;
+     function type(){
+      if(index < text.length){
+         texthii.textContent += text.charAt(index);
+         index++;
+         setTimeout(type, 150);
+
+      }
+     }
+     type();
+      
