9a6dbe0d0e04b30fe88a666b6fa92b75fc0a681e
Bipana <bipana4@gmail.com>
2025-05-20 10:05:32 +0545

    jyhtfy

--- Bipana.js
+++ Bipana.js
@@ -20,17 +20,21 @@
          {
             image:"image/b.jpg",
             title:"Animated Navbar",
-            description:"The blue flower symbolizes desire, love, and the metaphysical quest for the infinite or unreachable. It's often connected with a longing for the unknown or a spiritual journey—something just out of reach, but deeply yearned for."
+            description:"The blue flower symbolizes desire, love, and the metaphysical quest for the infinite or unreachable. It's often connected with a longing for the unknown or a spiritual journey—something just out of reach, but deeply yearned for.",
+            src: "https://bipana25.github.io/BMI/",
          },
          {
             image:"image/cloud.jpg",
             title:"landing page",
-            description:"Rabbits are playful and very loyal to their masters. They have a sharp sense of hearing and eyesight.They have long ears which can detect danger from a long distance."
+            description:"Rabbits are playful and very loyal to their masters. They have a sharp sense of hearing and eyesight.They have long ears which can detect danger from a long distance.", 
+            src: "https://bipana25.github.io/BMI/",
          },
          {
             image:"image/rabbit 2.jpg",
             title:"Animated Navbar",
-            description:"A dog has sharp teeth so that it can eat flesh very easily, it has four legs, two ears, two eyes, a tail, a mouth, and a nose. It runs very fast, barks loudly and attacks the strangers."
+            description:"A dog has sharp teeth so that it can eat flesh very easily, it has four legs, two ears, two eyes, a tail, a mouth, and a nose. It runs very fast, barks loudly and attacks the strangers.",
+            src:"https://bipana25.github.io/BMI",
+
          },
       ];
 
@@ -40,9 +44,10 @@
          const card =document.createElement("div");
          card.className ="card";
          card.innerHTML =`
-         <img src="${project.image}" alt="${project.title}" />
+         <img src="${project.image}" alt="${project.title}" \>
          <h3>${project.title}</h3>
          <p>${project.description}</p>
+         <a href="${project.src}">view project</a>
          `;
          container.appendChild(card);
       });
---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("bmiform").addEventListener("submit",
+    function (e)
+    {
+        e.preventDefault();
+        const height=document.getElementById("height").value;
+        const weight=document.getElementById("weight").value;
+        const result=document.getElementById("result");
+        if(!height || !weight || height <=0 || weight <=0) {
+            result.textContent ="please enter valid height and weight!";
+            return;
+        }
+        const heightInMeter =height /100;
+        const bmi=(weight /(heightInMeter *heightInMeter)).toFixed(2);
+        let category="";
+        if(bmi <18.5){
+            category ="Underweight";
+        } else if (bmi < 24.9){
+            category ="Normalweight";
+        } else if (bmi < 24.9){
+            category ="Overweight";
+        } else{
+            category ="Obesity";
+        }
+        result.textContent =`your BMI is ${bmi} ( ${category})`
+ });
