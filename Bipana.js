3d6ac5b527360ffdf76f627015fc1c1d3b239abd
Bipana <bipana4@gmail.com>
2025-05-23 08:41:01 +0545

    index added

--- Bipana.js
+++ Bipana.js
@@ -20,20 +20,22 @@
          {
             image:"image/b.jpg",
             title:"Animated Navbar",
-            description:"The blue flower symbolizes desire, love, and the metaphysical quest for the infinite or unreachable. It's often connected with a longing for the unknown or a spiritual journey—something just out of reach, but deeply yearned for.",
-            src: "https://bipana25.github.io/BMI/",
+            description:"The blue flower symbolizes desire, love, and the metaphysical quest for the infinite or unreachable. ",
+            src: " https://bipana25.github.io/BMI/",
+
          },
          {
             image:"image/cloud.jpg",
             title:"landing page",
-            description:"Rabbits are playful and very loyal to their masters. They have a sharp sense of hearing and eyesight.They have long ears which can detect danger from a long distance.", 
-            src: "https://bipana25.github.io/BMI/",
+            description:"Rabbits are playful and very loyal to their masters. They have a sharp sense of hearing and eyesight.", 
+            src:" https://bipana25.github.io/BMI/",
+
          },
          {
             image:"image/rabbit 2.jpg",
             title:"Animated Navbar",
-            description:"A dog has sharp teeth so that it can eat flesh very easily, it has four legs, two ears, two eyes, a tail, a mouth, and a nose. It runs very fast, barks loudly and attacks the strangers.",
-            src:"https://bipana25.github.io/BMI",
+            description:"A dog has sharp teeth so that it can eat flesh very easily, it has four legs, two ears, two eyes, a tail, a mouth, and a nose. ",
+            src:" https://bipana25.github.io/BMI/",
 
          },
       ];
